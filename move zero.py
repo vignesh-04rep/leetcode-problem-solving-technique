5e500db79efc99eb01001262a7f77c8b2f916367
vignesh <you.comm>
2025-01-11 13:27:10 +0530

    i done with my solution

--- move zero.py
+++ move zero.py
@@ -1,7 +1,23 @@
-nums =[0,1,0,3,12]
+#i done this problem with my own code but i did not statisfies all edgecase
+ def moveZeroes(self, nums):
+        null=0
+        for j in range(len(nums)-1,-1,-1):
+            break
+        for i in range(len(nums)): 
+            if nums[i] == null:
+                nums[i],nums[j] = nums[j],nums[i]
+                j-=1
+            if j == i:
+                break
+        return nums
+#the solution i submit
+class Solution:
+    def moveZeroes(self, nums):
+        left = 0
 
-for j in range(len(nums)-1,-1,-1):
-        break
-for i in range(0,5):
-        print(nums[j])
-        j-=1
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[right], nums[left] = nums[left], nums[right]
+                left += 1
+        
+        return nums
